89830835ea4afd52025f635507b4d4a2eb5bfaee
Zain Orwa <orwazain3@gmail.com>
2025-10-19 13:31:22 +0200

    ex00 ft_putchar

--- C00/ex01/README.md
+++
@@ -1,3 +0,0 @@
-
-![Exercise 01](./ex01.png)
-
--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -5,8 +5,9 @@ void ft_putchar(char c){
 }
 
 void ft_print_alphabet(void){
-	char letter = 'a';
+	char letter;
 
+	letter = 'a';
 	while (letter <= 'z'){
 		ft_putchar(letter);
 		letter++;	
