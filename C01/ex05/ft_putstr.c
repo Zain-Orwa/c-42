202947e6a8e918748115322efbd732b916e9cfcf
Zain Orwa <orwazain3@gmail.com>
2026-01-15 17:37:57 +0100

    ft_putstr

--- C01/ex05/ft_putstr.c
+++ C01/ex05/ft_putstr.c
@@ -1,17 +1,12 @@
 #include <unistd.h>
 
 void ft_putstr(char *str){
-	int i;
-
-	i = 0;
-	while (str[i]){
-		write(1, &str[i], 1);
-		i++;
-	}
+	while (*str)
+		write(1, str++, 1);
 }
 
 int main(){
-	char *str = {"print a string!"};
+	char *str = {"print a string!\n"};
 
 	ft_putstr(str);
 
