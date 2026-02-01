5b0d84eb0db88d98f8843d1c212f53c1eaadfef4
Orwa Zain <orwazain3@gmail.com>
2026-02-01 20:34:05 +0100

    ft_strlen.c

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -2,20 +2,19 @@
 #include <unistd.h>
 
 int ft_strlen(char *str){
-	int i;
+	char *start;
 
-	i = 0;
-	while (str[i])
-		i++;
-
-	return (i);
+	start = str;
+	while (*str)
+		str++;
+	return (str - start);
 }
 
 int main(){
 	char *str = {"strlen is counting"};
 	ft_strlen(str);
 
-	printf("Number of char in string: %d\n", ft_strlen(str));
+	printf("Number of characters in the string: %d\n", ft_strlen(str));
 
 	return (0);
 }
