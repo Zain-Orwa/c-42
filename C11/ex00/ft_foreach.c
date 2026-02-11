45fd4f3d7f3633b23968d544b48e445926451e53
Orwa Zain <orwazain3@gmail.com>
2026-02-11 20:08:53 +0100

    ft_map.c

--- C11/ex00/ft_foreach.c
+++ C11/ex00/ft_foreach.c
@@ -32,16 +32,21 @@ void ft_putnbr_space(int nb)
     ft_putchar(' ');
 }
 
-void ft_foreach(int *tab, int length, void(*f)(int))
+void	ft_foreach(int *tab, int length, void (*f)(int))
 {
-    int i;
-    
-    i = 0;
-    while (i < length)
-    {
-        f(tab[i]);
-        i++;
-    }
+	int	i;
+
+	// Safety check: Don't do anything if the pointers are NULL or length is invalid
+	if (!tab || !f || length <= 0)
+		return ;
+
+	i = 0;
+	while (i < length)
+	{
+		// Execute the function 'f' using the value at the current index
+		f(tab[i]);
+		i++;
+	}
 }
 
 int main(void)
@@ -65,3 +70,65 @@ int main(void)
     return (0);
 }
 
+/*
+#include <unistd.h>
+
+
+void	ft_foreach(int *tab, int length, void (*f)(int))
+{
+	int	i;
+
+	// Safety check: Don't do anything if the pointers are NULL or length is invalid
+	if (!tab || !f || length <= 0)
+		return ;
+
+	i = 0;
+	while (i < length)
+	{
+		// Execute the function 'f' using the value at the current index
+		f(tab[i]);
+		i++;
+	}
+}
+
+
+
+// A simple function to print an integer using write
+void	ft_putnbr(int n)
+{
+	long	nb;
+	char	c;
+
+	nb = n;
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
+
+// A wrapper to print the number followed by a space for readability
+void	print_item(int n)
+{
+	ft_putnbr(n);
+	write(1, " ", 1);
+}
+
+int	main(void)
+{
+	int		my_array[] = {42, 1337, 7, -5, 0, 100};
+	int		size = 6;
+
+	write(1, "Array contents: ", 16);
+
+	// Calling the exercise function with our array and our print function
+	ft_foreach(my_array, size, &print_item);
+
+	write(1, "\nTest Complete.\n", 16);
+	return (0);
+}
+*/
