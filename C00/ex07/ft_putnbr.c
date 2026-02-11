1849295a5af0767b71ab01b7862ec80a2c6a1e55
Orwa Zain <orwazain3@gmail.com>
2026-02-11 20:00:25 +0100

    ft_putnbr.c

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -22,6 +22,50 @@ void ft_putnbr(int nb){
 	ft_putchar((num % 10) + '0');
 }
 
+/*
+ * buffer version:
+ *
+ void	ft_putnbr(int n)
+{
+	long	nb;
+	char	buffer[12];  // Enough for INT_MIN + sign + null
+	int		i;
+	bool	is_negative;
+
+	nb = n;
+	is_negative = (nb < 0);
+	if (is_negative)
+		nb = -nb;
+	
+	// Handle zero case
+	if (nb == 0)
+	{
+		ft_putchar('0');
+		return;
+	}
+	
+	// Fill buffer from right to left
+	i = 11;
+	buffer[i] = '\0';
+	while (nb > 0)
+	{
+		i--;
+		buffer[i] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	
+	// Add sign if negative
+	if (is_negative)
+	{
+		i--;
+		buffer[i] = '-';
+	}
+	
+	// Write the entire number at once
+	write(1, &buffer[i], 11 - i);
+}
+*/
+
 int main(void){
 
 	write(1, "Print a positive number: ", 26);
