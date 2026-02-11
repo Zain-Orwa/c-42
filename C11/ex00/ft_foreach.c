743921fca52ba7958f87f13baa1fae3f490b126c
Orwa Zain <orwazain3@gmail.com>
2026-02-11 17:43:56 +0100

    ft_foreach.c

--- C11/ex00/ft_foreach.c
+++ C11/ex00/ft_foreach.c
@@ -2,7 +2,7 @@
 
 void ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
 
 void ft_putnbr(int nb)
@@ -26,10 +26,16 @@ void ft_putnbr(int nb)
         ft_putchar(nb + '0');
 }
 
+void ft_putnbr_space(int nb)
+{
+    ft_putnbr(nb);
+    ft_putchar(' ');
+}
+
 void ft_foreach(int *tab, int length, void(*f)(int))
 {
     int i;
-
+    
     i = 0;
     while (i < length)
     {
@@ -40,13 +46,22 @@ void ft_foreach(int *tab, int length, void(*f)(int))
 
 int main(void)
 {
-    int array[] = {42, -123, 0, 789, 2147483647, -2147483648};
-    int size = sizeof(array) / sizeof(array[0]);
-
-    ft_putchar('[');
-    ft_foreach(array, size, &ft_putnbr);
-    ft_putchar(']');
+    int array1[] = {1, 2, 3, 4, 5};
+    int array2[] = {-10, -20, -30};
+    int array3[] = {0};
+    
+    // Test 1: Print numbers without separator
+    ft_foreach(array1, 5, &ft_putnbr);
     ft_putchar('\n');
-
+    
+    // Test 2: Print numbers with space separator
+    ft_foreach(array2, 3, &ft_putnbr_space);
+    ft_putchar('\n');
+    
+    // Test 3: Single element array
+    ft_foreach(array3, 1, &ft_putnbr);
+    ft_putchar('\n');
+    
     return (0);
 }
+
