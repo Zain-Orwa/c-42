81631696571056bf2686c0fcd0a6a3e1b47bc3d1
Orwa Zain <orwazain3@gmail.com>
2026-02-23 06:56:58 +0100

    push front element in linked list

--- C12/ex00/ft_list.h
+++ C12/ex00/ft_list.h
@@ -2,9 +2,10 @@
 #ifndef FT_LIST_H
 #define FT_LIST_H
 
-typedef struct s_list {
-	struct s_list *next;
+typedef struct s_list 
+{
 	void *data;
+	struct s_list *next;
 } t_list;
 
 t_list	*ft_create_elem(void *data);
